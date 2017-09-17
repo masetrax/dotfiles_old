1d94d16db78f1083018cf72204c59a28c08bd3e0
Ryan Mason <masetrax1@gmail.com>
2017-09-18 06:04:54 +0800

    Updates

--- git_addons.sh
+++ git_addons.sh
@@ -1,4 +1,5 @@
 !# /bin/bash
 
-git clone https://github.com/aaronjamesyoung/Numix-Ocean
+git clone https://github.com/horst3180/arc-theme
 git clone https://github.com/meskarune/i3lock-fancy
+git clone https://github.com/stark/siji
